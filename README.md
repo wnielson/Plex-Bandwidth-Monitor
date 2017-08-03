e5dd0a78837263a868995543aec238125dcff82d
Weston Nielson <wnielson@gmail.com>
2017-08-02 18:34:48 -0700

    Large rewrite.  Now logs to database as well.

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
 # Plex-Bandwidth-Monitor
 Monitor bandwidth utilization on a per-stream basis.
 
-Currently, this is a working proof of concept but not really
-usable in its current form.  To test it out, read the install
-section below.
-
-
 ## Install
-You need to install ``pcap`` and the Python bindings.  On Ubuntu,
-the following should do it:
+Packets are captured via one of the following routes:
+
+  1. `pcap`, if it is installed, or
+  2. `AF_PACKET`
+
+`AF_PACKET` is only available on Linux (as far as I know), so if you are running on a different platform, try installing `pcap`.  On Ubuntu, the following should do it:
 
 ```
-sudo apt-get install libpcap0.8
-apt-get install python-pypcap
+sudo apt-get install python-pypcap libpcap0.8
 ```
 
 You also need to open ``plex_bwmon.py`` and set your ``PLEX_TOKEN``,
@@ -24,26 +22,31 @@ data about your streams.
 
 ## Known Limitations
 
-  1. Does not recognize transcoded streams yet (only direct play/stream
-     works at the moment).  This is a simple fix that will be addressed.
+  1. ~~Does not recognize transcoded streams yet (only direct play/stream
+     works at the moment).  This is a simple fix that will be addressed.~~
   2. If two clients from the same network (i.e. with the same public IP
      address) request the same file at the same time, then they will not
      be identified as separate streams.  This is due to the limited data
      available in the `PMS` logs.  I'm still working on a solution to
      this.
-  3. Occasionally the stream metadata lookup fails to grab the metadata
+  3. ~~Occasionally the stream metadata lookup fails to grab the metadata
      due to a race condition between when the stream is detected and when
      Plex reports the stream metadata (at the `/status/sessions` endpoint).
-     This should be pretty easy to fix.
-  4. Currently only works on Linux.  However, `libpcap` is available on
+     This should be pretty easy to fix.~~
+  4. Only tested on Linux.  However, `libpcap` is available on
      most platforms (including Windows via `WinPcap`), so cross-platform
      support should be fairly easily doable.
 
 
 ## Technical Details
 
-This works by listening to all outbound TCP traffic (using the `pcap`
-library).  When a new stream request is observed in the `PMS` logs, the
-destiation IP address **and port** are used to create a packet filter.
-As packets are sent to the client at that IP address and port, the bytes
-are counted and used to calculate the effective bandwidth every second.
+This works by listening to all outbound TCP traffic.  When a new stream request is observed on the Plex server the destination IP address is used to create a packet filter.
+As packets are sent to the client at that IP address, the bytes are counted and used to calculate the effective bandwidth.
+
+Example output:
+
+```
+[xxx.xxx.xxx.xxx] [directplay] [username] [ 3.31,  4.11,  4.34 Mbps] [movie: Movie Name]
+```
+
+The three sets of numbers before the `Mbps` are the average bandwidths for the last minute, 5 minutes and 10 minutes, respectively.
--- plex_bwmon.py
+++ plex_bwmon.py
@@ -1,88 +1,148 @@
 #!/usr/bin/env python
+import argparse
 import logging
 import os
 import re
 import socket
+import sqlite3
 import struct
+import sys
 import threading
 import time
+import traceback
 import urllib
+import urlparse
 
 try:
     from xml.etree import cElementTree as ET
 except:
     from xml.etree import ElementTree as ET
 
-import pcap
+try:
+    import pcap
+except:
+    pcap = None
 
+# 
 # TODO: Get this programatically
+# 
 PLEX_TOKEN = ""
-
-# TODO: Add support for transcoded streams
-STREAM_RE = re.compile(r"(Request|Completed): \[([\d\.]+)\:(\d+)\] GET \/library\/parts")
-
-# TODO: Use the correct file depending on the current platform
-LOG_FILE = "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Logs/Plex Media Server.log"
+PLEX_HOST  = ""
+DATABASE = 'data.db'
 
 # Ethernet header length.  Used for packet header parsing
-ETH_LENGTH = 14
+ETH_LENGTH  = 14
+ETH_P_ALL   = 3
+MTU         = 0xffff
+
+
+__version__ = "0.2.0"
+
+
+SCHEMA = """
+CREATE TABLE `sessions` (
+    `id` TEXT,
+    `user_id` INTEGER,
+    `user_title` TEXT,
+    `player_address` TEXT,
+    `player_device` TEXT,
+    `player_platform` TEXT,
+    `video_key` TEXT,
+    `part_decision` TEXT,
+    `video_title` TEXT,
+    `video_type` TEXT,
+    `bytes` INTEGER,
+    `bps`   REAL,
+    PRIMARY KEY(`id`)
+)"""
 
 log = logging.getLogger(__name__)
 
-def tail_f(file_name, interval=0.2):
-    """
-    Opens file ``file_name`` and yields lines from the end of the file as the
-    file is written to.  The result is similar to "tail -f" on *nix systems.
-    """
-    fh = open(file_name)
 
-    # Keep track of the file's inode.  When this changes, we know the file has
-    # changed (been deleted or renamed) as well
-    inode = os.fstat(fh.fileno()).st_ino
+class PacketSniffer:
+    """
+    Interface to sniff packets.  If libpcap is available, it will be used.
+    If libpcap isn't available, then we fallback to Linux-only AF_PACKET.
+    """
+    def __init__(self, iface):
+        self.ins = None
+        self.pc  = None
 
-    fh.seek(0, 2)
-    while True:
-        where = fh.tell()
-        line  = fh.readline()
-        if not line:
-            time.sleep(interval)
-            fh.seek(where)
+        if pcap is None:
+            self.ins = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
+            self.ins.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 2**30)
+            self.ins.bind((iface, ETH_P_ALL))
         else:
-            yield line
-        
-        # If the file that we're interested in has a new inode, then we need to
-        # stop to signal that the file needs to be re-opened
-        if os.stat(file_name).st_ino != inode:
-            log.info("File was removed: %s" % file_name)
-            break
+            self.pc = pcap.pcap(iface)
 
-def etree_to_dict(t):
-    """
-    Convert an ElementTree node ``t`` into a ``dict``.  Attributes are prefaced
-    with the "@" symbol.
-    """
-    d = {t.tag : map(etree_to_dict, t.getchildren())}
-    d.update(('@' + k, v) for k, v in t.attrib.iteritems())
-    d['text'] = t.text
-    return d
+    def sniff(self, callback):
+        if self.ins:
+            pkt, sa_ll = self.ins.recvfrom(MTU)
+            callback(0, pkt)
+        else:
+            self.pc.dispatch(cnt=256, callback=callback)
 
-def get_session(part_id):
+def update_sessions(sessions):
     """
-
+    Updates `sessions` to only include current sessions.  Returns a `dict` of
+    sessions that were removed (old sessions).
     """
-    res = urllib.urlopen("http://localhost:32400/status/sessions?X-Plex-Token=%s" % PLEX_TOKEN)
+    res = urllib.urlopen(urlparse.urljoin(PLEX_HOST, "/status/sessions?X-Plex-Token=%s" % PLEX_TOKEN))
     dom = ET.parse(res)
-    nodes = dom.findall(".//Video/Media/Part[@id='%s']" % part_id)
+    nodes = dom.findall(".//Video")
     log.info("Found nodes: %s" % nodes)
-    if len(nodes) == 1:
-        return etree_to_dict(nodes[0])
-    return None
+
+    current_session_ids = []
+
+    for node in nodes:
+        player = node.find("Player")
+        sesh   = node.find("Session")
+        user   = node.find("User")
+        part   = node.find("Media/Part")
+        trans  = node.find("TranscodeSession")
+
+        session_id = sesh.attrib.get('id')
+
+        if session_id not in sessions:
+            # New session
+            try:
+                sessions[session_id] = {
+                    'count':        0,
+                    'total_bytes':  0,
+                    'start':        time.time(),
+                    'averages':     [],
+                    'since':        time.time(),
+                    'data':         {
+                        "id":               sesh.attrib.get('id'),
+                        "user_id":          user.attrib.get('id'),
+                        "user_title":       user.attrib.get('title'),
+                        "player_address":   player.attrib.get('address'),
+                        "player_device":    player.attrib.get('device'),
+                        "player_platform":  player.attrib.get('platform'),
+                        "video_key":        node.attrib.get('key'),
+                        "part_decision":    part.attrib.get('decision'),
+                        "video_title":      node.attrib.get('title'),
+                        "video_type":       node.attrib.get('type')
+                    }
+                }
+            except Exception, e:
+                print "Error creating session:", e
+                continue
+
+        current_session_ids.append(session_id)
+
+    expired_sessions = {}
+    for sid in sessions.keys():
+        if sid not in current_session_ids:
+            expired_sessions[sid] = sessions.pop(sid)
+
+    return expired_sessions
 
 
 class BandwidthMonitorThread(threading.Thread):
     daemon = True
 
-    def __init__(self, iface=None, auto_start=True):
+    def __init__(self, iface='eth0', auto_start=True):
         threading.Thread.__init__(self)
 
         self.__halt    = False
@@ -98,85 +158,131 @@ class BandwidthMonitorThread(threading.Thread):
         s_addr = socket.inet_ntoa(iph[8])
         d_addr = socket.inet_ntoa(iph[9])
 
+        #print s_addr, d_addr
+
         counter = self._counters.get(d_addr, None)
         if counter is None:
             return
 
-        version_ihl = iph[0]
-        version = version_ihl >> 4
-        ihl = version_ihl & 0xF
-
-        iph_length = ihl * 4
-
-        t = iph_length + ETH_LENGTH
-        tcp_header = pkt[t:t+20]
-        tcph = struct.unpack('!HHLLBBHHH' , tcp_header)
-
-        dest_port = int(tcph[1])
-
-        counter = counter.get(dest_port, None)
-        if counter is None:
-            return
-
+        counter['total_bytes'] += len(pkt)
         counter['count'] += len(pkt)
+
         delta = time.time()-counter['since']
-        if delta >= 1:
+        if delta >= 60:
+            avg = counter['count']/delta
+            
+            if len(counter['averages']) >= 10:
+                counter['averages'].pop()
+            counter['averages'].insert(0, avg)
+
             #logger.debug("[%15s] %5.2f Mbps", d_addr, counter['count']/delta*8e-6)
-            name = ""
-            if counter["data"]:
-                name = "file='%s'" % os.path.basename(counter["data"]["@file"])
-            print "[%15s:%s] %5.2f Mbps %s" % (d_addr, dest_port, counter['count']/delta*8e-6, name)
+
+            print "[%15s] [%s] [%s] [%5.2f, %5.2f, %5.2f Mbps] [%s: %s]" % (
+                d_addr,
+                counter["data"]["part_decision"],
+                counter["data"]["user_title"],
+                (avg*8e-6),
+                sum(counter["averages"][:5])*8e-6/(len(counter["averages"][:5])),
+                sum(counter["averages"])*8e-6/(len(counter["averages"])),
+                counter["data"]["video_type"],
+                counter["data"]["video_title"])
+            
             counter['count'] = 0
             counter['since'] = time.time()
 
-    def add_address(self, address, port, data=None):
-        self._counters.setdefault(address,{})[int(port)] = {
-            'count': 0,
-            'since': time.time(),
-            'data':  data
-        }
+    def add_address(self, address, data):
+        if not address in self._counters:
+            self._counters[address] = data
+            return True
+        return False
 
-    def remove_address(self, address, port):
-        counter = self._counters.get(address, {})
-        counter.pop(int(port), None)
-        if len(counter) == 0:
-            self._counters.pop(address, None)
+    def remove_address(self, address):
+        self._counters.pop(address, None)
+
+    def get_addresses(self):
+        return self._counters.keys()
 
     def run(self):
-        #logger.info("Starting BandwidthMonitorThread")
-        pc = pcap.pcap(self.__iface)
+        sniffer = PacketSniffer(self.__iface)
+
         while not self.__halt:
             try:
-                pc.dispatch(cnt=256, callback=self.get_packet)
+                sniffer.sniff(self.get_packet)
+            except KeyboardInterrupt, e:
+                break
             except Exception, e:
+                #exc_type, exc_value, exc_traceback = sys.exc_info()
+                #traceback.print_tb(exc_traceback, file=sys.stdout)
                 #logger.error("Error: %s", str(e))
                 print "Error: %s", str(e)
-                time.sleep(2)
+                time.sleep(1)
+
         #logger.debug("Quitting")
         print "Quitting"
 
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+def get_db_conn():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = dict_factory
+
+    try:
+        conn.execute("SELECT COUNT(*) FROM sessions")
+    except:
+        conn.execute(SCHEMA)
+        conn.commit()
+
+    return conn
+
+def update_or_create_session(conn, session):
+    if session['count'] > 0:
+        data = session['data']
+        data.update({
+            'bytes': session.get('total_bytes', 0),
+            'bps':   session.get('total_bytes', 0)/((time.time()-session.get('start')) or 1),
+        })
+
+        keys         = data.keys()
+        fields       = ",".join(["`%s`" % f for f in keys])
+        placeholders = ",".join("?" for f in keys)
+        data         = [data[k] for k in keys]
+
+        conn.execute("REPLACE INTO sessions (%s) VALUES (%s)" % (fields, placeholders), data)
+        conn.commit()
+
 def run():
     monitor = BandwidthMonitorThread()
-    streams = {}
-    while True:
-        print "Opening log file: %s" % LOG_FILE
-        for line in tail_f(LOG_FILE):
-            m = STREAM_RE.search(line)
-            if m:
-                groups = m.groups()
-                media  = line.split("GET /library/parts/")[-1].split("/")[0]
-                key    = "%s:%s" % (groups[1], groups[2])
-                if groups[0] == "Request":
-                    streams[key] = media
-                    session = get_session(media) 
-                    monitor.add_address(groups[1], groups[2], session)
-                    get_session(media)
-                    print "Start stream [%d]: %s:%s %s" % (len(streams), groups[1], groups[2], media)
-                else:
-                    streams.pop(key, None)
-                    monitor.remove_address(groups[1], groups[2])
-                    print "End stream [%d]:   %s:%s %s" % (len(streams), groups[1], groups[2], media)
+    
+    conn = get_db_conn()
 
-                
+    sessions = {}
+    while True:
+
+        expired_sessions = update_sessions(sessions)
+        for session in sessions.values():
+            monitor.add_address(session['data']['player_address'], session)
+            update_or_create_session(conn, session)
+
+        for session in expired_sessions.values():
+            print "Removing address", session['data']['player_address']
+            monitor.remove_address(session['data']['player_address'])
+
+        time.sleep(5)
+        continue
+
+
 if __name__ == "__main__":
-    run()
+    parser = argparse.ArgumentParser(description='Monitor bandwidth usage of Plex')
+    
+    args = parser.parse_args()
+
+    try:
+        run()
+    except KeyboardInterrupt:
+        print "Bye!"
+
